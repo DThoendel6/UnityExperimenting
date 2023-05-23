9692384c6668f93e22740b0ab933808a1d44a977
DThoendel6 <61917002+DThoendel6@users.noreply.github.com>
2023-05-22 21:25:26 -0500

    Drew an image. Used a script to print a message

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Debug.Log("I'm Blue Da Ba Dee Da Ba Dai");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
